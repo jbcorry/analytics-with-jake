0f373c849ac4047ef5f43507b7769e61fc981de6
Jake Corry <jakecorry@jakes-mbp.lan>
2023-07-26 22:12:15 -0600

    change home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>
+            Welcome to Analytics with Jake!
+        </h1>
+    </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/analytics-with-jake',
   plugins: [
     vue(),
     vueJsx(),
